406b588d6d2c382e771a381826bd2695b1db5c5a
ivgnod <dongvt@ubox.vn>
2024-07-21 23:37:11 +0700

    update metadata

--- src/app/(home)/layout.jsx
+++ src/app/(home)/layout.jsx
@@ -5,6 +5,32 @@ import { LoadingFullPage } from '@/components/loading-full';
 import { ReactLenis } from '@/libs/lenis';
 import { Suspense } from 'react';
 
+export const metadata = {
+	title: 'Le Minh Quyen Portfolio',
+	description: 'Đây là trang web về Lê Minh Quyền',
+	keywords: '',
+	metadataBase: 'https://portfolio-ivgnod-v2.vercel.app',
+	openGraph: {
+		title: 'Le Minh Quyen Portfolio',
+		description: 'Đây là trang web về Lê Minh Quyền',
+		url: '',
+		images: {
+			url: '/images/hero-name2.png',
+			width: '100%',
+			height: '100%',
+		},
+	},
+	twitter: {
+		title: 'Le Minh Quyen Portfolio',
+		description: 'Đây là trang web về Lê Minh Quyền',
+		images: {
+			url: '/images/hero-name2.png',
+			width: '100%',
+			height: '100%',
+		},
+	},
+};
+
 export default function Layout({ children }) {
 	return (
 		<ReactLenis root>
